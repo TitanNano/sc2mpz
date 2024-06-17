82f34dc2af21a97a63a1ca1b4d509e38945d92c4
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2024-06-17 14:23:42 +0200

    Remove unnecessary Arcs

--- src/open_city_2k/tile.rs
+++ src/open_city_2k/tile.rs
@@ -2,10 +2,9 @@
 
 use super::bit_flags::BitFlags;
 use super::building::Building;
-use super::minimap::Minimap;
-use super::sc_util::int_to_bitstring;
+use super::City;
 use serde::Serialize;
-use std::fmt::Display;
+use std::ops::Deref;
 use std::sync::Arc;
 
 #[derive(Debug, Serialize)]
@@ -20,40 +19,14 @@ pub struct Tile {
     zone_corners: String,
     zone: u32,
     underground: u8,
-    _label: Vec<String>,
+    label: Vec<String>,
     text_pointer: i32,
     bit_flags: Option<BitFlags>,
-    #[serde(skip_serializing)]
-    _traffic_minimap: Arc<Minimap>,
-    #[serde(skip_serializing)]
-    _pollution_minimap: Arc<Minimap>,
-    #[serde(skip_serializing)]
-    _value_minimap: Arc<Minimap>,
-    #[serde(skip_serializing)]
-    _crime_minimap: Arc<Minimap>,
-    #[serde(skip_serializing)]
-    _police_minimap: Arc<Minimap>,
-    #[serde(skip_serializing)]
-    _fire_minimap: Arc<Minimap>,
-    #[serde(skip_serializing)]
-    _density_minimap: Arc<Minimap>,
-    #[serde(skip_serializing)]
-    _growth_minimap: Arc<Minimap>,
 }
 
 impl Tile {
     #[allow(clippy::too_many_arguments)]
-    pub fn new(
-        traffic: Arc<Minimap>,
-        pollution: Arc<Minimap>,
-        value: Arc<Minimap>,
-        crime: Arc<Minimap>,
-        police: Arc<Minimap>,
-        fire: Arc<Minimap>,
-        density: Arc<Minimap>,
-        growth: Arc<Minimap>,
-        label: Vec<String>,
-    ) -> Self {
+    pub fn new(label: Vec<String>) -> Self {
         let coordinates = (0, 0);
         // Altitude map related values.
         let altitude_tunnel = 0;
@@ -67,7 +40,6 @@ impl Tile {
         let zone_corners = String::from("");
         let zone = 0;
         let underground = 0;
-        let _label = label;
         // text/signs
         let text_pointer = -1;
         // bit flags
@@ -85,34 +57,46 @@ impl Tile {
             zone_corners,
             zone,
             underground,
-            _label,
+            label,
             text_pointer,
             bit_flags,
-            _traffic_minimap: traffic,
-            _pollution_minimap: pollution,
-            _value_minimap: value,
-            _crime_minimap: crime,
-            _police_minimap: police,
-            _fire_minimap: fire,
-            _density_minimap: density,
-            _growth_minimap: growth,
         }
+    }
+
+    pub fn coordinates(&self) -> (usize, usize) {
+        self.coordinates
     }
 
     pub fn set_coordinates(&mut self, value: (usize, usize)) {
         self.coordinates = value;
     }
 
+    pub fn altitude(&self) -> u32 {
+        self.altitude
+    }
+
     pub fn set_altitude(&mut self, value: u32) {
         self.altitude = value;
+    }
+
+    pub fn altitude_unknown(&self) -> u32 {
+        self.altitude_unknown
     }
 
     pub fn set_altitude_unknown(&mut self, value: u32) {
         self.altitude_unknown = value;
     }
 
+    pub fn altitude_tunnel(&self) -> u32 {
+        self.altitude_tunnel
+    }
+
     pub fn set_altitude_tunnel(&mut self, value: u32) {
         self.altitude_tunnel = value;
+    }
+
+    pub fn is_water(&self) -> bool {
+        self.is_water
     }
 
     pub fn set_is_water(&mut self, value: bool) {
@@ -151,8 +135,8 @@ impl Tile {
         self.underground = value;
     }
 
-    pub fn text_pointer(&self) -> &i32 {
-        &self.text_pointer
+    pub fn text_pointer(&self) -> i32 {
+        self.text_pointer
     }
 
     pub fn set_text_pointer(&mut self, value: u8) {
@@ -167,40 +151,40 @@ impl Tile {
         &self.bit_flags
     }
 
-    fn get_traffic(&self) -> &u8 {
-        self._traffic_minimap.get_scaled(self.coordinates)
+    pub fn traffic(&self, city: &City) -> u8 {
+        *city.traffic.get_scaled(self.coordinates)
     }
 
-    fn get_pollution(&self) -> &u8 {
-        self._pollution_minimap.get_scaled(self.coordinates)
+    pub fn pollution(&self, city: &City) -> u8 {
+        *city.pollution.get_scaled(self.coordinates)
     }
 
-    fn get_value(&self) -> &u8 {
-        self._value_minimap.get_scaled(self.coordinates)
+    pub fn value(&self, city: &City) -> u8 {
+        *city.value.get_scaled(self.coordinates)
     }
 
-    fn get_crime(&self) -> &u8 {
-        self._crime_minimap.get_scaled(self.coordinates)
+    pub fn crime(&self, city: &City) -> u8 {
+        *city.crime.get_scaled(self.coordinates)
     }
 
-    fn get_police(&self) -> &u8 {
-        self._police_minimap.get_scaled(self.coordinates)
+    pub fn police(&self, city: &City) -> u8 {
+        *city.police.get_scaled(self.coordinates)
     }
 
-    fn get_fire(&self) -> &u8 {
-        self._fire_minimap.get_scaled(self.coordinates)
+    pub fn fire(&self, city: &City) -> u8 {
+        *city.fire.get_scaled(self.coordinates)
     }
 
-    fn get_density(&self) -> &u8 {
-        self._density_minimap.get_scaled(self.coordinates)
+    pub fn density(&self, city: &City) -> u8 {
+        *city.density.get_scaled(self.coordinates)
     }
 
-    fn get_growth(&self) -> &u8 {
-        self._growth_minimap.get_scaled(self.coordinates)
+    pub fn growth(&self, city: &City) -> u8 {
+        *city.growth.get_scaled(self.coordinates)
     }
 
-    fn get_text(&self) -> &str {
-        &self._label[self.text_pointer as usize]
+    pub fn text(&self) -> Option<&str> {
+        self.label.get(self.text_pointer as usize).map(Deref::deref)
     }
 
     #[allow(dead_code)]
@@ -210,58 +194,5 @@ impl Tile {
 
     pub fn set_building(&mut self, value: Arc<Building>) {
         self.building = Some(value);
-    }
-}
-
-impl Display for Tile {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let terr = int_to_bitstring(self.terrain as u32, 0);
-        let b_id = if let Some(building) = &self.building {
-            format!("{:#04x}", building.building_id)
-        } else {
-            String::from("null")
-        };
-
-        let sign_text = if self.text_pointer > -1 {
-            format!(", Sign: {:?}", self.get_text())
-        } else {
-            String::from("")
-        };
-
-        write!(
-            f,
-            r#"Tile at {:?}
-Altitude:
-    tunnel: {}, water: {}, unknown: {}, altitude: {}
-Terrain: {}
-Buildings:
-    id: {}, corners {}, zone: {}, underground: {}
-Text pointer: {}{}
-Flags: {:?}
-Minimap:
-    Traffic: {:?}, pollution: {:?}, value: {:?}, crime: {:?}, police: {:?}, fire: {:?}, density: {:?}, growth: {:?}
-"#,
-            self.coordinates,
-            self.altitude_tunnel,
-            self.is_water,
-            self.altitude_unknown,
-            self.altitude,
-            terr,
-            b_id,
-            self.zone_corners,
-            self.zone,
-            self.underground,
-            self.text_pointer,
-            sign_text,
-            self.bit_flags,
-            self.get_traffic(),
-            self.get_pollution(),
-            self.get_value(),
-            self.get_crime(),
-            self.get_police(),
-            self.get_fire(),
-            self.get_density(),
-            self.get_growth()
-        )
     }
 }
